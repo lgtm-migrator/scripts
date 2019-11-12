999746b78cfce670717558235496d69397a4e570
Injabie3 <injabie3@gmail.com>
2019-11-11 17:45:07 -0800

    [Deresute] Add constants.py - Forgot to commit this last month.

---
+++ deresute/constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# 2019-10-11
+# Injabie3
+#
+# Description:
+# iDOLM@STER Cinderella Girls Starlight Stage
+# Constants file
+
+CSV_FILE = "data.csv"
+
+KEY_FAN = "fan"
+KEY_LEVEL = "level"
+KEY_PRP = "prp"
+KEY_TIMESTAMP = "timestamp"
+
+ROW_GRAPHS = [KEY_FAN, KEY_LEVEL, KEY_PRP]
+
+ROW_HEADER = \
+["timestamp", "name", "id", "comment", "level", "fan", "prp", "rank"]
+ROW_HEADER_CLEARED = \
+["clr_debut", "clr_light", "clr_pro", "clr_master", "clr_master_plus"]
+ROW_HEADER_FC = \
+["fc_debut", "fc_light", "fc_pro", "fc_master", "fc_master_plus"]
