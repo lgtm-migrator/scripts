937f4b3716b49eb247ca24907e62a282ccdb3a26
Injabie3 <injabie3@gmail.com>
2020-02-17 13:18:25 -0800

    [Anime NP] Clean up code
    - Fix spacing around variable declarations.
    - Create new variables for SQL queries using HEREDOC strings, and keep
      them separate from logic, since they're really long.
    - Fix braces, keep them on the same line as while/if/elseif statements.

--- anime-nowplaying/anime-nowplaying.php
+++ anime-nowplaying/anime-nowplaying.php
@@ -19,70 +19,68 @@ Last Modified: 2017-02-05 - Fixed "" around mal index.
 include_once( "anime-nowplaying-config.php" );
 
 // Connect to server and select database.
-$con = new mysqli("$host", "$username", "$password")or die("Cannot connect (Main)"); 
-$con->select_db("$dbName")or die(mysqli_error());
+$con = new mysqli("$host", "$username", "$password") or die("Cannot connect (Main)");
+$con->select_db("$dbName") or die(mysqli_error());
 $con->query("SET NAMES 'utf8'");
 
 // Get data from MAL Updater 
-$user=$_POST['user'];
-$animeID=$_POST['animeID'];
-$name=$_POST['name'];
-$ep=$_POST['ep'];
-$eptotal=$_POST['eptotal'];
-$picurl=$_POST['picurl'];
-$code=$_GET['code'];
+$user = $_POST['user'];
+$animeID = $_POST['animeID'];
+$name = $_POST['name'];
+$ep = $_POST['ep'];
+$eptotal = $_POST['eptotal'];
+$picurl = $_POST['picurl'];
+$code = $_GET['code'];
 
 
-// Insert data into mysql
-$data_mal=$con->query("SELECT * FROM  `$tableName` ORDER BY  `timestamp` DESC LIMIT 0 , 1") or die(mysqli_error());
-while($info_mal = $data_mal->fetch_array())
-{
-    if (isset($_GET['mal']) && $_GET['mal'] == "test")
-    {
-        $sql="INSERT INTO $tableName (`ID` ,`timestamp` ,`user` ,`animeID` ,`name` ,`ep` ,`eptotal` ,`picurl`) VALUES (NULL ,CURRENT_TIMESTAMP ,'Test', '0', 'Database Test', '0', '0', '');";
-        $result = $con->query($sql);
-        // if successfully insert data into database, displays message "Successful".
-        if($result)
-        {
+$sqlTest  = <<<SQL
+INSERT INTO $tableName
+(`ID`, `timestamp`, `user`, `animeID`, `name` ,`ep` ,`eptotal`,`picurl`)
+VALUES
+(NULL, CURRENT_TIMESTAMP, 'Test', '0', 'Database Test', '0', '0', '');
+SQL;
+
+$sqlInsert = <<<SQL
+INSERT INTO $tableName
+(`ID`, `timestamp`, `user`, `animeID`, `name` ,`ep` ,`eptotal`,`picurl`)
+VALUES
+(NULL, CURRENT_TIMESTAMP, '$user', '$animeID', '$name', '$ep', '$eptotal', '$picurl');
+SQL;
+
+$sqlGetData = <<<SQL
+SELECT * FROM `$tableName`
+ORDER BY `timestamp`
+DESC LIMIT 0, 1;
+SQL;
+
+// Get one result back: used to not post duplicate entries.
+$dataMAL=$con->query($sqlGetData) or die(mysqli_error());
+
+while ($infoMAL = $dataMAL->fetch_array()) {
+    if (isset($_GET['mal']) && $_GET['mal'] == "test") {
+        $result = $con->query($sqlTest);
+        if ($result) {
             echo "OK: Test update successful!";
             echo "<br>";
             echo "<a href='index.php'>Back to main page</a>";
-        }
-        else
-        {
+        } else {
             echo mysqli_error();
         }
-    }
-    elseif ($code != $usercode )
-    {
+    } elseif ($code != $usercode ) {
         echo "Error: Incorrect code. Please try again.";
-    }
-    elseif ($user == "")
-    {
+    } elseif ($user == "") {
         echo "Error: Please send data using MalUpdater!";
-    }
-    elseif ($name == "")
-    {
+    } elseif ($name == "") {
         echo "OK: Not playing anything, so no new information.  Not updating database.";
-    }
-    elseif ($name == $info_mal['name'] & $ep == $info_mal['ep'])
-    {
+    } elseif ($name == $infoMAL['name'] & $ep == $infoMAL['ep']) {
         echo "OK: Same information as last update.  Not updating database.";
-    }
-    else
-    {
-        $sql="INSERT INTO $tableName (`ID`,
-                    `timestamp` ,`user` ,`animeID` ,`name` ,`ep` ,`eptotal` ,`picurl`) VALUES (NULL ,CURRENT_TIMESTAMP ,'$user', '$animeID', '$name', '$ep', '$eptotal', '$picurl');";
-        $result = $con->query($sql);
-        // if successfully insert data into database, displays message "Successful"
-        if($result)
-        {
+    } else {
+        $result = $con->query($sqlInsert);
+        if ($result) {
             echo "OK: Update posted successfully";
             echo "<br>";
             echo "<a href='index.php'>Back to main page</a>";
-        }
-        else
-        {
+        } else {
             echo mysqli_error();
         }
     }
