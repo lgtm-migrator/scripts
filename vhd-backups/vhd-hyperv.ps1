a460322d57b8f0e159a3bdcdd8279b1a0c83d5a2
Injabie3 <injabie3@gmail.com>
2020-03-07 14:38:01 -0800

    [VHD Backups] Refactor Hyper-V backup script
    - Split long lines up for easier readability.
    - Convert individual destinations and VMs into arrays.
    - Change email font size to 11px.

--- vhd-backups/vhd-hyperv.ps1
+++ vhd-backups/vhd-hyperv.ps1
@@ -28,69 +28,70 @@ Discord-PostWebhook "Monthly Backups" ":information_source: Testing new script, 
 # Discord-PostWebhook-VANime "Monthly Backups" ":information_source: Kanbi will be shutting down momentarily..."
 
 # Stop the Virtual Machines.
-# TODO move VM names to settings file.
-#Stop-VM -Name "LuiV-Azuki"
-#Stop-VM -Name "LuiV-DC2" -Force
-#Stop-VM -Name "LuiV-GitLab"
-#Stop-VM -Name "LuiV-Silica"
-#Stop-VM -Name "LuiV-Testing"
+$VMsOff.ForEach({ echo "$_" })
+# $VMs.ForEach({ Stop-VM -Name "$_" })
 
-Discord-PostWebhook "Monthly Backups" ":information_source: VMs are now turned off.  Starting backups.."
-# Discord-PostWebhook-SFUAni "Monthly Backups" ":information_source: Ren is currently offline.  Starting backups..."
-# Discord-PostWebhook-VANime "Monthly Backups" ":information_source: Kanbi is currently offline.  Starting backups..."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: VMs are now turned off. Starting backups.."
+# Discord-PostWebhook "$($DiscordTitle)" `
+#     ":information_source: Ren is currently offline. Starting backups..."
+# Discord-PostWebhook "$($DiscordTitle)" `
+#     ":information_source: Kanbi is currently offline. Starting backups..."
 
 ### BACKUP PHASE 1 ###
 # Backup to RAID Mirror first.
-# Robocopy $SourceFolder $DestinationRAID /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
+# Robocopy $SourceFolder $DestinationMain /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
 
-Discord-PostWebhook "Monthly Backups" ":information_source: Phase 1 of backups complete (RAID)."
-# Discord-PostWebhook-SFUAni "Monthly Backups" ":information_source: The scheduled monthly backups have completed.  Ren will restart momentarily..."
-# Discord-PostWebhook-VANime "Monthly Backups" ":information_source: The scheduled monthly backups have completed.  Kanbi will restart momentarily..."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Phase 1 of backups complete (RAID)."
+# Discord-PostWebhook-SFUAni "$($DiscordTitle)" `
+#     ":information_source: Backups have completed. Ren should restart momentarily..."
+# Discord-PostWebhook-VANime "$($DiscordTitle)" `
+#     ":information_source: Backups have completed. Kanbi should restart momentarily..."
+Add-Content $Logfile "`r`nBackup Phase 1 completed, starting up VMs...`r`n"
 
-Add-Content $Logfile "`r`nBackup Phase 1 completed, starting up virtual machines...`r`n"
-
-### VM Reboot ###
 # Start up the Virtual Machines again, but stagger the startup times.
-Add-Content $Logfile "Booting up LuiV-Azuki"
-# Start-VM -Name "LuiV-Azuki"
-Add-Content $Logfile "[OK]`r`n"
-Add-Content $Logfile "Timeout period...`r`n"
+foreach($vm in $VMsOn) {
+    Add-Content $Logfile "Booting up $($vm)"
+    # Start-VM -Name $vm
+    Timeout 30
+}
 
-# TIMEOUT 30
+# Local Backups
+foreach( $dest in $DestinationsLocal ) {
+    Add-Content $Logfile "Backing up to $($dest[0])`r`n"
+    Discord-PostWebhook "$($DiscordTitle)" `
+        ":information_source: Backing up to $($dest[0])..."
+    #Robocopy $DestinationMain $dest[1] /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
+}
 
-Add-Content $Logfile "Booting up LuiV-Silca..."
-# Start-VM -Name "LuiV-Silica"
-Add-Content $Logfile "[OK]`r`n"
-Add-Content $Logfile "Timeout period...`r`n"
-
-# TIMEOUT 120
-
-Add-Content $Logfile "Starting Backup Phase 2...`r`n"
-
-Discord-PostWebhook "Monthly Backups" ":information_source: Starting phase 2 of backups (External HDDs)."
-
-### BACKUP PHASE 2 ###
-# Copy from the backup'd folder to other backup destinations.
-#Robocopy $DestinationRAID $DestinationFolder1 /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
-#Robocopy $DestinationRAID $DestinationFolder2 /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
-
-Discord-PostWebhook "Monthly Backups" ":information_source: Starting phase 3 of backups (LuiP-Miria)."
-# Robocopy $DestinationRAID $DestinationFileServer /E /B /R:10 /W:30 /LOG+:$Logfile /TEE /NP
+# Remote Backups
+foreach( $dest in $DestinationsRemote ) {
+    Add-Content $Logfile "Backing up to $($dest[0])`r`n"
+    Discord-PostWebhook "$($DiscordTitle)" `
+        ":information_source: Backing up to $($dest[0])..."
+    # When backing up over the network, do not use z flag or else we take a long
+    # time to back up over network.
+    #Robocopy $DestinationMain $dest /E /B /R:10 /W:30 /LOG+:$Logfile /TEE /NP
+}
 
 # Prep the email to be sent.
 $LogfileContents = Get-Content $Logfile | Out-String
-$EmailBodyComplete = "<pre style=""font-size:14px"">" + $LogfileContents + "</pre>"
+$EmailBodyComplete = "<pre style=""font-size:11px"">" + $LogfileContents + "</pre>"
 
 # Append to continuous log
 # Add-Content $LogfileAppend $LogfileContents
 
 # Send E-mail message with log file attachment
-$Message = New-Object Net.Mail.MailMessage($EmailFrom, $EmailTo1, $EmailSubject, $EmailBodyComplete)
+$Message = New-Object `
+    Net.Mail.MailMessage($EmailFrom, $EmailTo1, $EmailSubject, $EmailBodyComplete)
 $Message.IsBodyHtml = $true
 $SMTPClient = New-Object Net.Mail.SmtpClient("smtp.gmail.com", 587)
 $SMTPClient.EnableSsl = $true
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$SMTPClient.Credentials = New-Object `
+    System.Net.NetworkCredential($Username, $Password);
 
 #Send email 1 to mailing list.
 # $SMTPClient.Send($Message)
-Discord-PostWebhook "Monthly Backups" ":information_source: Monthly backups are now complete.  The status report was sent to `$($EmailTo1)`."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Backups are complete. The report was sent to `$($EmailTo1)`."
