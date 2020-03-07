44f6fb18a49aedffee2d489dd5eecc3befea3fa8
Injabie3 <injabie3@gmail.com>
2020-03-07 14:48:09 -0800

    [VHD Backups] Clean up
    - Uncomment necessary function calls to copy, power on/off VMs, and send
      webhook requests.
    - Shorten remaining lines.
    - Fix missing index in $dest.

--- vhd-backups/vhd-hyperv.ps1
+++ vhd-backups/vhd-hyperv.ps1
@@ -23,37 +23,38 @@ echo "======================================================================="
 echo "= This script will backup the Virtual machines on this server.        ="
 echo "======================================================================="
 
-Discord-PostWebhook "Monthly Backups" ":information_source: Testing new script, please ignore."
-# Discord-PostWebhook-SFUAni "Monthly Backups" ":information_source: Ren will be shutting down momentarily..."
-# Discord-PostWebhook-VANime "Monthly Backups" ":information_source: Kanbi will be shutting down momentarily..."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Turning off VMs..."
+Discord-PostWebhook-SFUAni "$($DiscordTitle)" `
+    ":information_source: Ren will be shutting down momentarily..."
+Discord-PostWebhook-VANime "$($DiscordTitle)" `
+    ":information_source: Kanbi will be shutting down momentarily..."
 
 # Stop the Virtual Machines.
-$VMsOff.ForEach({ echo "$_" })
-# $VMs.ForEach({ Stop-VM -Name "$_" })
+$VMsOff.ForEach({ Stop-VM -Name "$_" })
 
 Discord-PostWebhook "$($DiscordTitle)" `
     ":information_source: VMs are now turned off. Starting backups.."
-# Discord-PostWebhook "$($DiscordTitle)" `
-#     ":information_source: Ren is currently offline. Starting backups..."
-# Discord-PostWebhook "$($DiscordTitle)" `
-#     ":information_source: Kanbi is currently offline. Starting backups..."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Ren is currently offline. Starting backups..."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Kanbi is currently offline. Starting backups..."
 
-### BACKUP PHASE 1 ###
-# Backup to RAID Mirror first.
-# Robocopy $SourceFolder $DestinationMain /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
+# Back up to main destination first.
+Robocopy $SourceFolder $DestinationMain /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
 
 Discord-PostWebhook "$($DiscordTitle)" `
     ":information_source: Phase 1 of backups complete (RAID)."
-# Discord-PostWebhook-SFUAni "$($DiscordTitle)" `
-#     ":information_source: Backups have completed. Ren should restart momentarily..."
-# Discord-PostWebhook-VANime "$($DiscordTitle)" `
-#     ":information_source: Backups have completed. Kanbi should restart momentarily..."
+Discord-PostWebhook-SFUAni "$($DiscordTitle)" `
+    ":information_source: Backups have completed. Ren should restart momentarily..."
+Discord-PostWebhook-VANime "$($DiscordTitle)" `
+    ":information_source: Backups have completed. Kanbi should restart momentarily..."
 Add-Content $Logfile "`r`nBackup Phase 1 completed, starting up VMs...`r`n"
 
 # Start up the Virtual Machines again, but stagger the startup times.
 foreach($vm in $VMsOn) {
     Add-Content $Logfile "Booting up $($vm)"
-    # Start-VM -Name $vm
+    Start-VM -Name $vm
     Timeout 30
 }
 
@@ -62,7 +63,7 @@ foreach( $dest in $DestinationsLocal ) {
     Add-Content $Logfile "Backing up to $($dest[0])`r`n"
     Discord-PostWebhook "$($DiscordTitle)" `
         ":information_source: Backing up to $($dest[0])..."
-    #Robocopy $DestinationMain $dest[1] /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
+    Robocopy $DestinationMain $dest[1] /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
 }
 
 # Remote Backups
@@ -72,7 +73,7 @@ foreach( $dest in $DestinationsRemote ) {
         ":information_source: Backing up to $($dest[0])..."
     # When backing up over the network, do not use z flag or else we take a long
     # time to back up over network.
-    #Robocopy $DestinationMain $dest /E /B /R:10 /W:30 /LOG+:$Logfile /TEE /NP
+    Robocopy $DestinationMain $dest[1] /E /B /R:10 /W:30 /LOG+:$Logfile /TEE /NP
 }
 
 # Prep the email to be sent.
@@ -80,7 +81,7 @@ $LogfileContents = Get-Content $Logfile | Out-String
 $EmailBodyComplete = "<pre style=""font-size:11px"">" + $LogfileContents + "</pre>"
 
 # Append to continuous log
-# Add-Content $LogfileAppend $LogfileContents
+Add-Content $LogfileAppend $LogfileContents
 
 # Send E-mail message with log file attachment
 $Message = New-Object `
@@ -91,7 +92,7 @@ $SMTPClient.EnableSsl = $true
 $SMTPClient.Credentials = New-Object `
     System.Net.NetworkCredential($Username, $Password);
 
-#Send email 1 to mailing list.
-# $SMTPClient.Send($Message)
+# Send E-mail to admin.
+$SMTPClient.Send($Message)
 Discord-PostWebhook "$($DiscordTitle)" `
-    ":information_source: Backups are complete. The report was sent to `$($EmailTo1)`."
+    ":information_source: Backups are complete. The report was sent to `$($EmailTo)`."
