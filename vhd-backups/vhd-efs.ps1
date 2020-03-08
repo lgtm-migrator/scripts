8e3f4ea8b11e3f49d341463380c5ebd912ea43df
Injabie3 <injabie3@gmail.com>
2020-03-07 16:53:48 -0800

    [VHD Backups] Refactor
    - Split long lines up for easier readability.
    - Convert individual destinations and VHDs into arrays.
    - Change email font size to 11px.

--- vhd-backups/vhd-efs.ps1
+++ vhd-backups/vhd-efs.ps1
@@ -12,48 +12,52 @@
 # Include the Discord helpers
 ."D:\Scripts\Helpers\Discord-Webhook.ps1"
 
-# Change these values
-$BackupDate = Get-Date -format "yyyy-MM-dd"
-$SourceFolder = "G:\EFS"
-$DestinationFolder1 = "H:\Backups\EFS\$BackupDate" #2TB
-$DestinationFolder2 = "I:\Backups\EFS\$BackupDate" #3TB
-$DestinationFolder3 = "E:\Backups\EFS\$BackupDate" #RAID mirror
-$Logfile = "G:\Logs\EFSCopy-Last.txt"
-$LogfileAppend = "G:\Logs\EFSCopy.txt"
-$EmailFrom = "no-reply@injabie3.tk"
-$EmailTo = "sysadmin@injabie3.tk"
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Dismounting VHDs..."
+$VHDDismount.ForEach( { Dismount-VHD -Path "$($_[1])" } )
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: VHDs are dismounted. Starting backups.."
 
-$ComputerName = (Get-CIMInstance CIM_ComputerSystem).Name
-$EmailSubject = "${ComputerName}: Weekly Backup Log - EFS: $BackupDate"
-$Username = "no-reply@injabie3.tk"
-$Password = "<password here>"
+Robocopy $SourceFolder $DestinationMain[1] /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
 
-Discord-PostWebhook -title "Monthly Backups" -message ":information_source: The scheduled VHD monthly backups have started. Dismounting VHDs..."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Phase 1 complete, remounting VHDs..."
+$VHDDismount.ForEach( { Mount-VHD -Path "$($_[1])" } )
 
-#Dismount the VHDs
-."D:\Scripts\VHD-Dismount.ps1"
+# Local Backups
+foreach( $dest in $DestinationsLocal ) {
+    Add-Content $Logfile "Backing up to $($dest[0])`r`n"
+    Discord-PostWebhook "$($DiscordTitle)" `
+        ":information_source: Backing up to $($dest[0])..."
+    Robocopy $DestinationMain[1] $dest[1] /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
+}
 
-# Copy Folder with Robocopy
-Robocopy $SourceFolder $DestinationFolder1 /E /ZB /R:10 /W:30 /LOG:$Logfile /TEE /NP
-Robocopy $SourceFolder $DestinationFolder2 /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
-Robocopy $SourceFolder $DestinationFolder3 /E /ZB /R:10 /W:30 /LOG+:$Logfile /TEE /NP
+# Remote Backups
+foreach( $dest in $DestinationsRemote ) {
+    Add-Content $Logfile "Backing up to $($dest[0])`r`n"
+    Discord-PostWebhook "$($DiscordTitle)" `
+        ":information_source: Backing up to $($dest[0])..."
+    # When backing up over the network, do not use z flag or else we take a long
+    # time to back up over network.
+    Robocopy $DestinationMain[1] $dest[1] /E /B /R:10 /W:30 /LOG:$Logfile /TEE /NP
+}
 
-#Mount the VHDs again
-."D:\Scripts\VHD-Mount.ps1"
-
-#Prep the email to be sent.
+# Prep the email to be sent.
 $LogfileContents = Get-Content $Logfile | Out-String
-$EmailBodyComplete = "<pre style=""font-size:14px"">" + $LogfileContents + "</pre>"
+$EmailBodyComplete = "<pre style=""font-size:11px"">" + $LogfileContents + "</pre>"
 
-#Append to continuous log
+# Append to continuous log
 Add-Content $LogfileAppend $LogfileContents
 
 # Send E-mail message with log file attachment
-$Message = New-Object Net.Mail.MailMessage($EmailFrom, $EmailTo, $EmailSubject, $EmailBodyComplete)
+$Message = New-Object `
+    Net.Mail.MailMessage($EmailFrom, $EmailTo, $EmailSubject, $EmailBodyComplete)
 $Message.IsBodyHtml = $true
 $SMTPClient = New-Object Net.Mail.SmtpClient("smtp.gmail.com", 587)
 $SMTPClient.EnableSsl = $true
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$SMTPClient.Credentials = New-Object `
+    System.Net.NetworkCredential($Username, $Password);
 $SMTPClient.Send($Message)
 
-Discord-PostWebhook -title "Monthly Backups" -message ":information_source: VHD backups are now complete. The status report was sent to `sysadmin@injabie3.tk`."
+Discord-PostWebhook "$($DiscordTitle)" `
+    ":information_source: Backups have completed. The report was sent to `$($EmailTo)`."
