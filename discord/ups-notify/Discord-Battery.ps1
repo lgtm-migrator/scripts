bf5a7a654fa057ffd824c1ec62a049c48ce30541
Injabie3 <injabie3@gmail.com>
2020-03-08 15:42:25 -0700

    [Discord UPS] Move emoji from title to description

--- discord/ups-notify/Discord-Battery.ps1
+++ discord/ups-notify/Discord-Battery.ps1
@@ -30,7 +30,7 @@ $triggeredLow = $false
 $triggeredCritical = $false
 $triggeredFullyCharged = $false
 
-Discord-PostWebhook ":information_source: Discord Battery Script" "The battery checker script has been started."
+Discord-PostWebhook "Discord Battery Script" ":information_source: The battery checker script has been started."
 
 # Event loop.
 do
@@ -40,31 +40,32 @@ do
     $BatteryPercentage = $Battery.EstimatedChargeRemaining
 
 
-    if (($Status -eq 1) -and ($BatteryPercentage -le 25) -and (-not $triggeredLow))
+    if (($Status -eq 1) -and ($BatteryPercentage -le 35) -and (-not $triggeredLow))
     #Low Battery
     {
-        Discord-PostWebhook ":warning: :battery: Battery Low" "The server has $BatteryPercentage% battery left!  Shutdown will commence soon!"
+        Discord-PostWebhook "Battery Low" ":warning: :battery: The server has $BatteryPercentage% battery left!  Shutdown will commence soon!"
         $triggeredLow = $true
         $triggeredFullyCharged = $false
     }
     elseif(($Status -eq 1) -and (-not $triggered))
     # Battery is discharging.
     {
-        Discord-PostWebhook ":warning: :battery: Power Outage" "The server has switched to battery power ($BatteryPercentage% remaining)!"
+        Discord-PostWebhook "Power Outage" ":warning: :battery: The server has switched to battery power ($BatteryPercentage% remaining)!"
         $triggered = $true
         $triggeredFullyCharged = $false
     }
     elseif (($Status -eq 2) -and $triggered)
     # Back on AC.
     {
-        Discord-PostWebhook ":information_source: :electric_plug: Power Online" "The server has switched to AC with $BatteryPercentage% remaining."
+        Discord-PostWebhook "Power Online" ":information_source: :electric_plug: The server has switched to AC with $BatteryPercentage% remaining."
         $triggered = $false
         $triggeredLow = $false
+        shutdown /a /m \\LuiP-Miria
     }
     elseif (($Status -eq 2) -and ($BatteryPercentage -eq 100) -and (-not $triggeredFullyCharged))
     # Battery fully charged!
     {
-        Discord-PostWebhook ":information_source: :electric_plug: Battery Recharged" "The battery is now fully charged!"
+        Discord-PostWebhook "Battery Recharged" ":information_source: :electric_plug: The battery is now fully charged!"
         $triggeredFullyCharged = $true
     }
 
