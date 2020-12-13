129047771a58417595425bea4127896b8fec12be
Injabie3 <injabie3@gmail.com>
2020-12-12 18:35:59 -0800

    [DSLR Capture] Begin moving DSLR code to dslr.py
    - Create DSLR class.
    - Add fetchBatteryStatus method.
    - Add test case for method.

---
+++ dslr-capture/dslr.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python4
+# 2019-07-09
+# Injabie3
+#
+# Description:
+# Autocapture script for DSLR cameras. Used for SFU Anime stuff.
+# 
+from copy import deepcopy
+from datetime import datetime
+import logging
+from re import search
+import subprocess
+import sys
+
+from PIL import Image, ImageDraw, ImageFont
+import requests
+
+import constants
+
+class DSLR(object):
+    """Capture DSLR images and place a timestamp on them."""
+
+    def __init__(self, logger: logging.Logger, webhook: str=None):
+        self.sentAlert25 = False
+        self.sentAlertLow = False
+        self.batteryStatus = None
+        self.logger = logger
+        self.webhook = webhook
+        self.embed = deepcopy(constants.EMBED)
+
+    def fetchBatteryStatus(self):
+        batteryStatus = subprocess.run(["gphoto2", "--get-config=/main/status/batterylevel"],
+                                         stdout=subprocess.PIPE)
+        if batteryStatus.returncode != 0:
+            self.logger.error("Could not fetch battery status!")
+            self.batteryStatus = -1
+            return
+        level = search(r"(?:Current: )(\d+)(?:%)", batteryStatus.stdout).group(1)
+        self.batteryStatus = int(level)
+        self.logger.info("DSLR battery status: %s", batteryStatus)
---
+++ dslr-capture/test_dslr.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import dslr
+import unittest
+from unittest.mock import MagicMock, patch
+import logging
+import subprocess
+
+class TestDslr(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.Logger("Test")
+        self.dslr = dslr.DSLR(self.logger) 
+
+    def testFetchBatteryStatusThatIsUnableToFetch(self):
+        subRun = MagicMock(subprocess.run)
+        subRun.return_value = subprocess.CompletedProcess(["test"], -1)
+        assert self.dslr.batteryStatus == None, "Failed sanity check"
+        with patch("dslr.subprocess.run", subRun):
+            self.dslr.fetchBatteryStatus()
+            assert self.dslr.batteryStatus == -1, "We should have set battery status to -1!"
+
+    def testFetchBatteryStatusPercentages(self):
+        text = "Label: Battery Level\nReadonly: 0\nType: TEXT\nCurrent: {}%"
+        assert self.dslr.batteryStatus == None, "Failed sanity check"
+        for level in [ 100, 75, 50, 25, 0 ]:
+            subRun = MagicMock(subprocess.run)
+            subRun.return_value = subprocess.CompletedProcess(["test"], returncode=0,
+                                                              stdout=text.format(level))
+            with patch("dslr.subprocess.run", subRun):
+                self.dslr.fetchBatteryStatus()
+                assert self.dslr.batteryStatus == level, f"Battery level doesn't match {level}"
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
