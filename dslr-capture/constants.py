2369266aad8082180b92035c950c982dbdeb9a23
Injabie3 <injabie3@gmail.com>
2020-12-12 20:02:42 -0800

    [DSLR Capture] Add constants.py

---
+++ dslr-capture/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+EMBED = \
+{"username": "LuiP-RenPi",
+ "embeds":
+ [{"title": "DSLR script",
+   "description": "",
+   "color": 16723712,
+   "footer": {"text": "Autocapture script by Injabie3" }
+  }
+ ]
+}
