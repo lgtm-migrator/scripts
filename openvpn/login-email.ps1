d9c4f9b6acf4904bf9d6d46eddd2f21ba560e4c4
Injabie3 <injabie3@gmail.com>
2021-09-12 18:42:28 -0700

    [OpenVPN] Add login email script

---
+++ openvpn/login-email.ps1
@@ -0,0 +1,25 @@
+﻿# 2021-09-12
+# Injabie3
+#
+# Description:
+# OpenVPN Server Login Notifier
+
+# Fetch settings
+."$PSScriptRoot\login-email-settings.ps1"
+
+# Send E-mail message with the incoming connection info
+$EmailBodyComplete = @"
+Certificate CN: $Env:common_name
+Connected to: $ComputerName
+Origin: $Env:untrusted_ip
+Timestamp: $Env:time_ascii
+"@
+
+$Message = New-Object `
+    Net.Mail.MailMessage($EmailFrom, $EmailTo, $EmailSubject, $EmailBodyComplete)
+$Message.IsBodyHtml = $false
+$SMTPClient = New-Object Net.Mail.SmtpClient("smtp.gmail.com", 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object `
+    System.Net.NetworkCredential($Username, $Password);
+$SMTPClient.Send($Message)
